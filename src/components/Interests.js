5ec3fc0814b659d78b68cbdaacd09f0728b06b5f
FacuTombesi <facundomtombesi@gmail.com>
2024-07-04 17:40:40 -0300

    updated interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -5,11 +5,26 @@ export default function Interests() {
     <section className={styles.interests}>
       <h2 className={styles.title}><span className={styles.title_capital}>I</span>nterests</h2>
       <ul className={styles.list}>
-        <li className={styles.list_item}>Programming</li>
-        <li className={styles.list_item}>Video games</li>
-        <li className={styles.list_item}>Tennis</li>
-        <li className={styles.list_item}>Anime</li>
-        <li className={styles.list_item}>Music</li>
+        <li className={styles.list_item}>
+          <img className={styles.item_icon} src="/assets/icons_interest_1.png" alt="Programming" />
+          Programming
+        </li>
+        <li className={styles.list_item}>
+          <img className={styles.item_icon} src="/assets/icons_interest_2.png" alt="Video games" />
+          Video games
+        </li>
+        <li className={styles.list_item}>
+          <img className={styles.item_icon} src="/assets/icons_interest_3.png" alt="Tennis" />
+          Tennis
+        </li>
+        <li className={styles.list_item}>
+          <img className={styles.item_icon} src="/assets/icons_interest_4.png" alt="Japan" />
+          Japan
+        </li>
+        <li className={styles.list_item}>
+          <img className={styles.item_icon} src="/assets/icons_interest_5.png" alt="Music" />
+          Music
+        </li>
       </ul>
     </section>
   )
